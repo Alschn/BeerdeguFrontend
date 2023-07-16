81151835a3b95789837dc3714ec81734fafe0517
Alschn <67019937+Alschn@users.noreply.github.com>
2023-07-16 23:47:59 +0200

    Add auth routes layout

---
+++ src/components/auth/Header.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Group, Header, type HeaderProps } from "@mantine/core";
+
+const AuthHeader = (props: Omit<HeaderProps, "children">) => {
+  return (
+    <Header px="md" withBorder={false} pos="fixed" zIndex={1} {...props}>
+      <Group position="apart" align="center" h="100%">
+        {/* todo */}
+      </Group>
+    </Header>
+  );
+};
+
+export default AuthHeader;
