4d6607fc0bef8d8a2d31b1fa833f0d0f0fd15d23
Alschn <67019937+Alschn@users.noreply.github.com>
2023-07-17 16:44:57 +0200

    Add homepage related components with fake data (work in progress)

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,44 @@
+"use client";
+import type { ReactNode } from "react";
+import HomeFooter from "~/components/home/Footer";
+import HomeHeader from "~/components/home/Header";
+
+const footerLinks = [
+  {
+    title: "About",
+    links: [
+      { label: "About", link: "/about" },
+      { label: "Contact", link: "/contact" },
+      { label: "Privacy Policy", link: "/privacy-policy" },
+      { label: "Terms of Service", link: "/terms-of-service" },
+    ],
+  },
+  {
+    title: "Support",
+    links: [
+      { label: "Help Center", link: "/help-center" },
+      { label: "FAQ", link: "/faq" },
+      { label: "Contact", link: "/contact" },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { label: "Facebook", link: "https://facebook.com" },
+      { label: "Twitter", link: "https://twitter.com" },
+      { label: "Instagram", link: "https://instagram.com" },
+    ],
+  },
+];
+
+export default function HomeLayout({ children }: { children: ReactNode }) {
+  const headerHeight = 60;
+
+  return (
+    <>
+      <HomeHeader height={headerHeight} />
+      <main style={{ marginTop: headerHeight }}>{children}</main>
+      <HomeFooter data={footerLinks} />
+    </>
+  );
+}
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,16 @@
+import FrequentlyAskedQuestions from "~/components/home/FAQ";
+import Features from "~/components/home/Features";
+import Welcome from "~/components/home/Welcome";
+
+export default function HomePage() {
+  return (
+    <>
+      <Welcome />
+      <Features
+        title="Integrate effortlessly with any technology stack"
+        description="Every once in a while, you'll see a Golbat that's missing some fangs. This happens when hunger drives it to try biting a Steel-type Pokémon."
+      />
+      <FrequentlyAskedQuestions />
+    </>
+  );
+}
