8bd440cd13c452b74e7f913a8c9a93f0ebb833a3
Alschn <67019937+Alschn@users.noreply.github.com>
2023-07-21 19:44:59 +0200

    Add "auto saving" with loading overlay, extract stepper header to component

--- src/components/dashboard/room/states/InProgress.tsx
+++ src/components/dashboard/room/states/InProgress.tsx
@@ -6,61 +6,57 @@ import {
   Text,
   Divider,
   rem,
+  LoadingOverlay,
 } from "@mantine/core";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useRoom } from "~/components/context/room";
 import BeerForm from "../BeerForm";
 import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
 import type { BeerObject } from "~/api/types";
 import BeerDisplay from "../BeerDisplay";
 
+const SAVING_TIMEOUT = 500;
+
 const InProgress = () => {
   const { beers } = useRoom();
   const [activeStep, setActiveStep] = useState(0);
-
-  const totalBeers = beers.length;
+  const [isAutoSaving, setIsAutoSaving] = useState(false);
 
   const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setIsAutoSaving(true);
+    setTimeout(() => {
+      setIsAutoSaving(false);
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    }, SAVING_TIMEOUT);
   };
 
   const handlePrevious = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    setIsAutoSaving(true);
+    setTimeout(() => {
+      setIsAutoSaving(false);
+      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    }, SAVING_TIMEOUT);
   };
 
-  const currentBeer = beers[activeStep] as BeerObject;
+  const currentBeer = useMemo(() => {
+    return beers[activeStep] as BeerObject;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [beers, activeStep, isAutoSaving]);
+
+  const totalBeers = beers.length;
 
   if (totalBeers === 0) return null;
 
   return (
     <Container id="inprogress-container">
-      <Flex
-        align="center"
-        justify="space-between"
-        mb={16}
+      <LoadingOverlay id="inprogress-overlay" visible={isAutoSaving} />
+      <StepperHeader
+        activeStep={activeStep}
+        totalSteps={totalBeers}
+        onPrevious={handlePrevious}
+        onNext={handleNext}
         id="inprogress-stepper"
-      >
-        <Button
-          leftIcon={<IconArrowLeft size={rem(16)} />}
-          disabled={activeStep === 0}
-          onClick={handlePrevious}
-          id="inprogress-stepper-previous"
-        >
-          Previous
-        </Button>
-        <Text size="xl" weight={700} id="inprogress-stepper-label">
-          {activeStep + 1} / {totalBeers}
-        </Text>
-        <Button
-          rightIcon={<IconArrowRight size={rem(16)} />}
-          disabled={activeStep === totalBeers - 1}
-          onClick={handleNext}
-          id="inprogress-stepper-next"
-        >
-          Next
-        </Button>
-      </Flex>
-
+      />
       <Card withBorder id="inprogress-main">
         <BeerDisplay beer={currentBeer} />
         <Divider my={16} />
@@ -69,5 +65,53 @@ const InProgress = () => {
     </Container>
   );
 };
+
+interface StepperHeaderProps {
+  id?: string;
+  activeStep: number;
+  totalSteps: number;
+  onPrevious?: () => void;
+  onNext?: () => void;
+  previousLabel?: string;
+  nextLabel?: string;
+  showTopLabel?: boolean;
+}
+
+function StepperHeader({
+  activeStep,
+  totalSteps,
+  onNext,
+  onPrevious,
+  id,
+  previousLabel = "Previous",
+  nextLabel = "Next",
+  showTopLabel = true,
+}: StepperHeaderProps) {
+  return (
+    <Flex align="center" justify="space-between" mb={16} id={id}>
+      <Button
+        leftIcon={<IconArrowLeft size={rem(16)} />}
+        disabled={activeStep === 0}
+        onClick={onPrevious}
+        id="inprogress-stepper-previous"
+      >
+        {previousLabel}
+      </Button>
+      {showTopLabel && (
+        <Text size="xl" weight={700} id="inprogress-stepper-label">
+          {activeStep + 1} / {totalSteps}
+        </Text>
+      )}
+      <Button
+        rightIcon={<IconArrowRight size={rem(16)} />}
+        disabled={activeStep === totalSteps - 1}
+        onClick={onNext}
+        id="inprogress-stepper-next"
+      >
+        {nextLabel}
+      </Button>
+    </Flex>
+  );
+}
 
 export default InProgress;
