d6d7195a25bc32e218063b822584e122a37f46f0
Alschn <67019937+Alschn@users.noreply.github.com>
2023-08-03 12:05:14 +0200

    Add temporary dashboard browser layout

--- src/app/dashboard/(general)/browser/layout.tsx
+++ src/app/dashboard/(general)/browser/layout.tsx
@@ -1,7 +1,30 @@
+"use client";
+import { Box, Card, Tabs } from "@mantine/core";
+import { usePathname, useRouter } from "next/navigation";
 import { type FC, type ReactNode } from "react";
 
 const DashboardBrowserLayout: FC<{ children: ReactNode }> = ({ children }) => {
-  return <>{children}</>;
+  const pathname = usePathname();
+  const router = useRouter();
+  const path = pathname.split("/dashboard/browser/")[1];
+
+  const handleRouteChange = (value: string) => {
+    router.push(`/dashboard/browser/${value}`);
+  };
+
+  return (
+    <Card shadow="xs" p={0}>
+      <Tabs value={path} onTabChange={handleRouteChange}>
+        <Tabs.List grow>
+          <Tabs.Tab value="beers">Beers</Tabs.Tab>
+          <Tabs.Tab value="styles">Beer Styles</Tabs.Tab>
+          <Tabs.Tab value="hops">Hops</Tabs.Tab>
+          <Tabs.Tab value="breweries">Breweries</Tabs.Tab>
+        </Tabs.List>
+      </Tabs>
+      <Box component="section">{children}</Box>
+    </Card>
+  );
 };
 
 export default DashboardBrowserLayout;
