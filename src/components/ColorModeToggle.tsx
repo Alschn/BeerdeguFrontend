8653005c386f57652f002efa16de55ac3a2d24bb
Alschn <67019937+Alschn@users.noreply.github.com>
2024-05-11 16:59:39 +0200

    Major bump all possible packages
    Packages that cannot be upgraded right now (have breaking changes):
    - eslint 9
    - mantine 7
    - @tanstack/query 5

--- src/components/ColorModeToggle.tsx
+++ src/components/ColorModeToggle.tsx
@@ -2,18 +2,18 @@
 
 import {
   ActionIcon,
-  type ActionIconProps,
   useMantineColorScheme,
+  type ActionIconProps,
 } from "@mantine/core";
 import {
   IconMoonStars,
   IconSun,
-  type TablerIconsProps,
+  type IconProps as TablerIconProps
 } from "@tabler/icons-react";
 import { type FC } from "react";
 
 interface ColorModeToggleProps extends ActionIconProps {
-  iconProps?: TablerIconsProps;
+  iconProps?: Omit<TablerIconProps, "ref">;
 }
 
 const ColorModeToggle: FC<ColorModeToggleProps> = ({ iconProps, ...rest }) => {
@@ -32,17 +32,17 @@ const ColorModeToggle: FC<ColorModeToggleProps> = ({ iconProps, ...rest }) => {
       size={44}
       sx={(theme) => ({
         backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
+          theme.colorScheme === "dark"
+            ? theme.colors.dark[6]
+            : theme.colors.gray[0],
         color:
           theme.colorScheme === "dark"
             ? theme.colors.yellow[4]
             : theme.colors.blue[6],
-            "&:hover": {
-              backgroundColor:
-                theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-            },
+        "&:hover": {
+          backgroundColor:
+            theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
+        },
       })}
       {...rest}
     >
