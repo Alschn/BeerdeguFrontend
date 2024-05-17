0b157e107653ed38a3e902db4718808761479e41
Alschn <67019937+Alschn@users.noreply.github.com>
2024-05-17 15:45:37 +0200

    Fix existing jest tests

--- __tests__/jest/Welcome.spec.tsx
+++ __tests__/jest/Welcome.spec.tsx
@@ -1,12 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 import Welcome from "~/components/home/Welcome";
+import { wrapper } from "./utils";
 
-describe('Welcome component', () => {
-  it('has correct Next.js theming section link', () => {
-    render(<Welcome/>);
-    expect(screen.getByText('this guide')).toHaveAttribute(
-      'href',
-      'https://mantine.dev/guides/next/'
-    );
+jest.mock("next/navigation", () => ({
+  useRouter: () => {},
+}));
+
+describe("Welcome component", () => {
+  it("has correct Next.js theming section link", () => {
+    render(<Welcome />, { wrapper });
+    expect(screen.getByText("Beerdegu")).toBeInTheDocument();
   });
 });
